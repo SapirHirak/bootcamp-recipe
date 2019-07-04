6558a4a1f6f081f228d3f22ea7a21c1905cf9a5f
Sapir Hirak <sapirosh1h@gmail.com>
2019-07-04 12:40:33 +0300

    second

---
+++ server.js
@@ -0,0 +1,36 @@
+const request = require('request')
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, 'node_modules')))
+
+app.get('/sanity', function (req, respond) {
+    respond.send("OK")
+
+})
+
+
+app.get('/recipes/:ingredient', function (req, respond) {
+    let ingredient = req.params.ingredient //will be the ingredient name
+    let releventData = []
+    console.log(ingredient)
+    request.get(`https://recipes-goodness.herokuapp.com/recipes/${ingredient}`, function (err, res ) { //for full API
+
+        let data = JSON.parse( res.body).results
+        //players = players.filter(p => p.teamId == teamToIDs[player] & p.isActive)
+        for (let u of data) {
+            releventData.push({ title: u.title })
+        }
+  
+    respond.send(data)
+})
+})
+
+const port = 8080
+app.listen(port, function () {
+    console.log("server is running on port " + port)
+})
+
+
